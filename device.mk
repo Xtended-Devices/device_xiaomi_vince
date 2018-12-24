d801cfb51715ed5478806ebcc8fc81d27496e6cb
Rajkale99 <kaleraj.rk@gmail.com>
2018-12-24 19:38:21 +0100

    Nuke s2s vince display td4310 doesnt support that
    Signed-off-by: Raj Kale <kaleraj.rk@gmail.com>

--- device.mk
+++ device.mk
@@ -158,8 +158,8 @@ PRODUCT_PACKAGES += \
     vendor.display.color@1.0-impl
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
+#PRODUCT_PACKAGES += \
+#    XiaomiDoze
 
 # DRM
 PRODUCT_PACKAGES += \
